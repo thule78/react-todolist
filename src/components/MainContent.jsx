f9c66a77d891cd4d9fc6d7cad9f1907e80a57d5d
thule <lunthu@gmail.com>
2019-05-03 19:38:04 +0700

    using class

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import todosData from './TodoData'
 import TodoItem from './TodoItem'
 
-function MainContent(){
-  const date = new Date()
-  const yyyy =  date.getFullYear()
-  const mm = ("0"+(date.getMonth()+1)).slice(-2)
-  const dd = ("0"+date.getDate()).slice(-2)
-  const getdate = dd+'-'+mm+'-'+yyyy
-  const hours = date.getHours()
-  let timeOfDay
+class MainContent extends Component {
+  render(){
+    const date = new Date()
+    const yyyy =  date.getFullYear()
+    const mm = ("0"+(date.getMonth()+1)).slice(-2)
+    const dd = ("0"+date.getDate()).slice(-2)
+    const getdate = dd+'-'+mm+'-'+yyyy
+    const hours = date.getHours()
+    let timeOfDay
     if (hours < 12){
       timeOfDay = "Morning"
     } else if (hours >=12 && hours < 17){
@@ -18,25 +19,26 @@ function MainContent(){
     }else {
       timeOfDay = "Evening"
     }
-  const todoItems =todosData.map((item)=>{
+    const todoItems =todosData.map((item)=>{
+      return(
+        <TodoItem key={item.id} item={item} />
+        )
+    })
+
     return(
-      <TodoItem key={item.id} item={item} />
+      <div>
+        <div>
+          <p>Today: {`${getdate}`}</p>
+          <h3>Your todo list of this {`${timeOfDay}`}</h3>
+        </div>
+        <div>
+          {todoItems}
+        </div>
+      </div>
+
       )
-  })
+  }
 
-
-  return(
-    <div>
-      <div>
-        <p>Today: {`${getdate}`}</p>
-        <h3>Your todo list of this {`${timeOfDay}`}</h3>
-      </div>
-      <div>
-        {todoItems}
-      </div>
-    </div>
-
-    )
 }
 
 export default MainContent
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
-function TodoItem(props){
-  return(
+// function TodoItem(props){
+//   return(
+//     <div className="todo-item">
+//     <input type="checkbox"  checked={props.item.completed}/>
+//     <p>{props.item.text}</p>
+//     </div>
+
+//     )
+// }
+
+class TodoItem extends React.Component {
+  render(){
+    return(
     <div className="todo-item">
-    <input type="checkbox"  checked={props.item.completed}/>
-    <p>{props.item.text}</p>
+    <input type="checkbox"  checked={this.props.item.completed}/>
+    <p>{this.props.item.text}</p>
     </div>
 
     )
+  }
 }
 
 export default TodoItem
